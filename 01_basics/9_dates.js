d514ea83cf0c51353b214d7ecba8ef419c31f027
BasheerAhamedShaik <113755210+BasheerAhamedShaik@users.noreply.github.com>
2024-08-02 17:54:58 +0000

    Changes done

--- 01_basics/9_dates.js
+++ 01_basics/9_dates.js
@@ -19,5 +19,34 @@ console.log(mydate1.toLocaleString());      // 1/3/2024, 12:00:00 AM
 let mydate2 = new Date('01-03-2024');       // (mm/dd/yyyy) -> indian system
 console.log(mydate1.toLocaleString());      // 1/3/2024, 12:00:00 AM
 
+let time = Date.now()           
+console.log(time);                   // 1722619586525  (millisec value since 1970 jan 1)
+console.log(mydate2.getTime());      // 1704240000000
+
+console.log(mydate.getMilliseconds());  // 0
+
+console.log(Math.floor(time/1000))    // 1722620494 (sec value since 1970 jan 1)
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate());             // 2 (today is Aug 2)
+console.log(newDate.getDay());             // 5 (Friday)
+console.log(newDate.getFullYear());        // 2024
+console.log(newDate.getHours());           // 17
+console.log(newDate.getMonth());           // 7  ( since in JS month numbering starts from 0. Add 1 to avoid confusion)
+console.log(newDate.getMonth() + 1)        // 8
+console.log(newDate.getMilliseconds());    // 629
+
+console.log(newDate.toLocaleString('default', {    // Friday
+    weekday: "long",
+}))
+
+
+
+
+
+
+
+
 
 
