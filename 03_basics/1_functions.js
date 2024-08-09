361649f4879b30f11436eb949f44e34b6b9be481
BasheerAhamedShaik <113755210+BasheerAhamedShaik@users.noreply.github.com>
2024-08-09 18:49:48 +0000

    Changes done

--- 03_basics/1_functions.js
+++ 03_basics/1_functions.js
@@ -40,8 +40,49 @@ console.log(loginUserMessage());          // undefined just logged in
 console.log(loginUserMessage(""));        //  just logged in
 console.log(loginUserMessage("stark"));   // this overwrites Tony
 
-function calCartPrice(num){
+
+// Functions with objects and arrays
+
+function calCartPrice(...num){   // rest operator
     return num
 }
 
-console.log(calCartPrice(2));    // 2
+console.log(calCartPrice(200, 300, 500));        // [ 200, 300, 500 ]
+
+function calCartPrice(val1, val2, ...num){       // rest operator
+    return num
+}
+
+console.log(calCartPrice(200, 300, 500, 1000));  // [ 500, 1000 ]
+
+const user = {
+    id: 13,
+    desig: "Engineer"
+}
+
+function handleObject(anyobject){
+    console.log(`UserId is ${anyobject.id} and Designation is ${anyobject.desig}`);
+}
+
+// handleObject(user)       // UserId is 13 and Designation is Engineer
+
+// *** here in case suppose that in place of anyobject.id if we write anyobject.name then output will be 
+// "undefined is 13 and Designation is Engineer" so this is the drawback of JS we need to check by writing more code
+
+// instead of creating the object separetly we can pass the object directly also
+
+handleObject({
+    id:12,
+    desig: "IT"        // UserId is 12 and Designation is IT
+})
+
+// ** Arrays
+const myArray = [2,3,4,5,6]
+
+function one(getArray){
+    return getArray[0]
+}
+
+console.log(one(myArray));          // 2
+console.log(one([7,8,9,10,11]));    // 7
+
