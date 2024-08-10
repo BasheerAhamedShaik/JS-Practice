b2b0af1174feebd76dbe976b1d91c88f6a0285c3
BasheerAhamedShaik <113755210+BasheerAhamedShaik@users.noreply.github.com>
2024-08-10 18:23:20 +0000

    changes done

---
+++ 03_basics/2_scope.js
@@ -0,0 +1,15 @@
+var c = 300
+let a = 100
+// whatever we write values in block scope that can't be available outside and viceversa is possible
+
+if(true){             // outside of this if scope(block scope) is called Global scope
+    let a = 10
+    const b = 20
+    var c = 30        // or c=30
+}
+
+// console.log(a);  // ReferenceError: a is not defined
+// console.log(b);  // ReferenceError: b is not defined
+console.log(c);     // 30 ( this is the problem with var) if we declare again another value of c also it will print 30
+
+console.log(a);     // 100
