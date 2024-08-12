f5bec19273a202b4e646a7dda0af7c080465fbde
BasheerAhamedShaik <113755210+BasheerAhamedShaik@users.noreply.github.com>
2024-08-12 18:37:12 +0000

    changes done

--- 03_basics/2_scope.js
+++ 03_basics/2_scope.js
@@ -24,8 +24,39 @@ function one(){
         const platform = "Youtube"
         console.log(uName);
     }
-    // console.log(platform);            // first error is encountered here. Because we can't access the child function variable outside
+    // console.log(platform);       // first error is encountered here. Because we can't access the child function variable outside
     
     two()
 }
 one()         // Stark
+
+if(true){
+    const u2 = "Tony"
+    if(true){
+        const p2 = "Twitter"
+        console.log(u2 + p2);
+    }
+    // console.log(p2);        // first error is encountered here
+}
+// console.log(u2);        // here you can't access the variable outside the scope of "if"
+
+
+
+// *********************** Declaring functions methods ****************** //
+
+
+console.log(addOne(3))    // we can call the function before declaration also for this type of declaration
+
+function addOne(num){
+    return num + 1
+}
+
+// addOne(3)
+
+// console.log(addTwo(6))   // ReferenceError: Cannot access 'addTwo' before initialization. Because here we are declaring a function and holding it in the variable("Hoisting")
+
+const addTwo = function(num){
+    return num+2
+}
+
+// addTwo(6)
